ffbb5c51c8b13fc28a0e0044ff619177ceb91a57
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-03-28 02:31:49 +0530

    first commit in the client side where i have added the dependencies for the project for the front end

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
