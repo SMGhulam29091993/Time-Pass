e143f0e79541d8682d61f311d27de91f8891fd33
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-04-19 01:30:36 +0530

    added details to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import AppLayout from '../components/Layout/AppLayout'
+import { List, ListItem, Stack, Typography } from '@mui/material';
+import React from 'react';
+import AppLayout from '../components/Layout/AppLayout';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <>
+      <Typography p={"2rem"} variant='h1' textAlign={'center'} >Home</Typography>
+      <Stack sx={{display:"flex", alignItems:"center", justifyContent:"center" }} spacing={1} >
+        <List sx={{ padding: '16px', backgroundColor: '#f0f0f0' }}>
+          <ListItem sx={{ marginBottom: '8px' }}>
+            <Typography  sx={{ textDecoration: 'underline' }}>Select a friend to chat</Typography>
+          </ListItem>
+          <ListItem sx={{ marginBottom: '8px' }}>
+            <Typography  sx={{ textDecoration: 'underline' }}>Deleting a chat will lead to unfriending too.</Typography>
+          </ListItem>
+          <ListItem>
+            <Typography  sx={{ textDecoration: 'underline' }}>Share your QR to let others find your profile</Typography>
+          </ListItem>
+      </List>
+      </Stack>
+    </>
+    
   )
 }
 
