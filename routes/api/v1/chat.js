40b9667cd0355e5b3379bfadd618dd0d18d0fc79
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-06-19 15:41:47 +0530

    created get message and chat validator

--- routes/api/v1/chat.js
+++ routes/api/v1/chat.js
@@ -2,36 +2,39 @@ const express = require("express");
 const { isAuthenticated } = require("../../../middlewares/authentication");
 const chatController = require("../../../controllers/chatController.js");
 const { attachmentsMulter } = require("../../../middlewares/multer.js");
-const { validateHandler, newGroupValidator, addMemberValidator, removeMemberValidator, leaveGroupValidator, sendAttachmentvalidator } = require("../../../lib/validator.js");
+const { validateHandler, newGroupValidator, addMemberValidator, 
+        removeMemberValidator, leaveGroupValidator, sendAttachmentvalidator, getChatIDValidator, 
+        renameGroupValidator} = require("../../../lib/validator.js");
 
 
 
 const router = express.Router();
 
 
-router.post("/new-groupChat",newGroupValidator(),validateHandler, isAuthenticated,  chatController.newGroupChat);
+router.post("/new-groupChat", isAuthenticated,newGroupValidator(),validateHandler, chatController.newGroupChat);
 router.get("/getChat", isAuthenticated, chatController.getChats);
 router.get("/getGroupChat", isAuthenticated, chatController.getGroupChat);
-router.put("/addMembers",addMemberValidator(),validateHandler, isAuthenticated, chatController.addMembers);  
-router.put("/removeMembers",removeMemberValidator(),validateHandler, isAuthenticated, chatController.removeMembers);
-router.delete("/leaveGroup/:chatID",leaveGroupValidator(), validateHandler, isAuthenticated, chatController.leaveGroup);
+router.put("/addMembers", isAuthenticated,addMemberValidator(),validateHandler, chatController.addMembers);  
+router.put("/removeMembers", isAuthenticated,removeMemberValidator(),validateHandler, chatController.removeMembers);
+router.delete("/leaveGroup/:chatID", isAuthenticated,leaveGroupValidator(), validateHandler, chatController.leaveGroup);
 
 
 
 
 // Messages
-router.get("/messages/:chatID", isAuthenticated,chatController.getMessages);
+router.get("/messages/:chatID", isAuthenticated,getChatIDValidator(),validateHandler, chatController.getMessages);
 
 
 
 
 // Attechments
-router.post("/message-attachments",sendAttachmentvalidator(),validateHandler, isAuthenticated, attachmentsMulter, chatController.sendAttachments)
+router.post("/message-attachments", isAuthenticated, attachmentsMulter,sendAttachmentvalidator(),validateHandler, chatController.sendAttachments)
 
 
 
 // get chat details,rename,delete
-router.route("/:chatID").all(isAuthenticated).get(chatController.getChatDetails)
-                                            .put(chatController.renameGroup).delete(chatController.deleteChat);
+router.route("/:chatID").all(isAuthenticated).get(getChatIDValidator(),validateHandler,chatController.getChatDetails)
+                                            .put(renameGroupValidator(),validateHandler, chatController.renameGroup)
+                                            .delete(chatController.deleteChat);
 
 module.exports = router;
